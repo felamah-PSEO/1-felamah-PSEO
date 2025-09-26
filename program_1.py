82c038b6b5c118630a0775b5593a66f88c4950e6
felamah-PSEO <felamah@students.unwsp.edu>
2025-09-26 14:58:31 -0500

    Assignment 1

--- program_1.py
+++ program_1.py
@@ -1,6 +1,11 @@
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+
+    # Author - Faith
+    # Date - 09/01/2025
+    # Date - 09/01/25
+    # Title - Program A: Modified Hello Part I.
+    print('Hello World')
 
 hello_world()
--- program_2.py
+++ program_2.py
@@ -1,6 +1,14 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('...')
-    
+    # Author - Faith Lamah
+    # Date - 09/01/25
+    # Date - 09/01/2025
+    # Title - Program A: Modified Hello Part II.
+
+    print('“Do you see a man skillful in his work?')
+    print('He will stand before kings;')
+    print('he will not stand before obscure men.”')
+    print('(Proverb 22:29)')
+
 
 proverbs_22_29()
